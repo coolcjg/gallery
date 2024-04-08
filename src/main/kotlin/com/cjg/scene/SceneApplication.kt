ca2ae0e3f988b83a9863eb1e347b477746cc1661
coolcjg <coolcjg@naver.com>
2024-04-08 22:41:15 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/com/cjg/scene/SceneApplication.kt
@@ -0,0 +1,11 @@
+package com.cjg.scene
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SceneApplication
+
+fun main(args: Array<String>) {
+    runApplication<SceneApplication>(*args)
+}
---
+++ src/test/kotlin/com/cjg/scene/SceneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cjg.scene
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SceneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
